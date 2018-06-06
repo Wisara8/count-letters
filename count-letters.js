d50ee3ffb8dab4459ca33eb2610ecd8bb6085c9b
Wisara8 <srandall845@gmail.com>
2018-06-06 20:42:32 +0000

    count-letters hurray

--- count-letters.js
+++ count-letters.js
@@ -1,1 +1,15 @@
-//count letters
+function countLetters(string) {
+  var noSpaces = string.split(" ").join("");
+  var stats = {};
+
+  for (var i = 0; i < noSpaces.length; i++){
+    var currentLetter = noSpaces[i];
+    if (!stats[currentLetter]) {
+          stats[currentLetter] = 0;
+        }
+      stats[currentLetter]++
+      }
+console.log(stats);
+ }
+
+countLetters("lighthouse in the house");
